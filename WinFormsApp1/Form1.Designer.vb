283ea73e2b21135cebf9409684ddec3eed36902b
Jeric <jbbimol27@gmail.com>
2022-06-03 23:49:16 +0800

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.space1 = New System.Windows.Forms.Button()
+        Me.space2 = New System.Windows.Forms.Button()
+        Me.space3 = New System.Windows.Forms.Button()
+        Me.space6 = New System.Windows.Forms.Button()
+        Me.space5 = New System.Windows.Forms.Button()
+        Me.space4 = New System.Windows.Forms.Button()
+        Me.space9 = New System.Windows.Forms.Button()
+        Me.space8 = New System.Windows.Forms.Button()
+        Me.space7 = New System.Windows.Forms.Button()
+        Me.playerxlabel = New System.Windows.Forms.Label()
+        Me.playerolabel = New System.Windows.Forms.Label()
+        Me.xscore = New System.Windows.Forms.Label()
+        Me.oscore = New System.Windows.Forms.Label()
+        Me.resetbutton = New System.Windows.Forms.Button()
+        Me.newgamebutton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'space1
+        '
+        Me.space1.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.space1.Location = New System.Drawing.Point(180, 171)
+        Me.space1.Name = "space1"
+        Me.space1.Size = New System.Drawing.Size(70, 70)
+        Me.space1.TabIndex = 0
+        Me.space1.UseVisualStyleBackColor = True
+        '
+        'space2
+        '
+        Me.space2.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.space2.Location = New System.Drawing.Point(256, 171)
+        Me.space2.Name = "space2"
+        Me.space2.Size = New System.Drawing.Size(70, 70)
+        Me.space2.TabIndex = 1
+        Me.space2.UseVisualStyleBackColor = True
+        '
+        'space3
+        '
+        Me.space3.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.space3.Location = New System.Drawing.Point(332, 171)
+        Me.space3.Name = "space3"
+        Me.space3.Size = New System.Drawing.Size(70, 70)
+        Me.space3.TabIndex = 2
+        Me.space3.UseVisualStyleBackColor = True
+        '
+        'space6
+        '
+        Me.space6.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.space6.Location = New System.Drawing.Point(332, 247)
+        Me.space6.Name = "space6"
+        Me.space6.Size = New System.Drawing.Size(70, 70)
+        Me.space6.TabIndex = 5
+        Me.space6.UseVisualStyleBackColor = True
+        '
+        'space5
+        '
+        Me.space5.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.space5.Location = New System.Drawing.Point(256, 247)
+        Me.space5.Name = "space5"
+        Me.space5.Size = New System.Drawing.Size(70, 70)
+        Me.space5.TabIndex = 4
+        Me.space5.UseVisualStyleBackColor = True
+        '
+        'space4
+        '
+        Me.space4.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.space4.Location = New System.Drawing.Point(180, 247)
+        Me.space4.Name = "space4"
+        Me.space4.Size = New System.Drawing.Size(70, 70)
+        Me.space4.TabIndex = 3
+        Me.space4.UseVisualStyleBackColor = True
+        '
+        'space9
+        '
+        Me.space9.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.space9.Location = New System.Drawing.Point(332, 323)
+        Me.space9.Name = "space9"
+        Me.space9.Size = New System.Drawing.Size(70, 70)
+        Me.space9.TabIndex = 8
+        Me.space9.UseVisualStyleBackColor = True
+        '
+        'space8
+        '
+        Me.space8.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.space8.Location = New System.Drawing.Point(256, 323)
+        Me.space8.Name = "space8"
+        Me.space8.Size = New System.Drawing.Size(70, 70)
+        Me.space8.TabIndex = 7
+        Me.space8.UseVisualStyleBackColor = True
+        '
+        'space7
+        '
+        Me.space7.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.space7.Location = New System.Drawing.Point(180, 323)
+        Me.space7.Name = "space7"
+        Me.space7.Size = New System.Drawing.Size(70, 70)
+        Me.space7.TabIndex = 6
+        Me.space7.UseVisualStyleBackColor = True
+        '
+        'playerxlabel
+        '
+        Me.playerxlabel.AutoSize = True
+        Me.playerxlabel.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.playerxlabel.Location = New System.Drawing.Point(12, 9)
+        Me.playerxlabel.Name = "playerxlabel"
+        Me.playerxlabel.Size = New System.Drawing.Size(189, 54)
+        Me.playerxlabel.TabIndex = 9
+        Me.playerxlabel.Text = "Player X:"
+        '
+        'playerolabel
+        '
+        Me.playerolabel.AutoSize = True
+        Me.playerolabel.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.playerolabel.Location = New System.Drawing.Point(12, 63)
+        Me.playerolabel.Name = "playerolabel"
+        Me.playerolabel.Size = New System.Drawing.Size(193, 54)
+        Me.playerolabel.TabIndex = 10
+        Me.playerolabel.Text = "Player O:"
+        '
+        'xscore
+        '
+        Me.xscore.AutoSize = True
+        Me.xscore.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.xscore.Location = New System.Drawing.Point(207, 9)
+        Me.xscore.Name = "xscore"
+        Me.xscore.Size = New System.Drawing.Size(46, 54)
+        Me.xscore.TabIndex = 11
+        Me.xscore.Text = "0"
+        '
+        'oscore
+        '
+        Me.oscore.AutoSize = True
+        Me.oscore.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.oscore.Location = New System.Drawing.Point(207, 63)
+        Me.oscore.Name = "oscore"
+        Me.oscore.Size = New System.Drawing.Size(46, 54)
+        Me.oscore.TabIndex = 12
+        Me.oscore.Text = "0"
+        '
+        'resetbutton
+        '
+        Me.resetbutton.Location = New System.Drawing.Point(332, 17)
+        Me.resetbutton.Name = "resetbutton"
+        Me.resetbutton.Size = New System.Drawing.Size(100, 100)
+        Me.resetbutton.TabIndex = 13
+        Me.resetbutton.Text = "Reset"
+        Me.resetbutton.UseVisualStyleBackColor = True
+        '
+        'newgamebutton
+        '
+        Me.newgamebutton.Location = New System.Drawing.Point(457, 17)
+        Me.newgamebutton.Name = "newgamebutton"
+        Me.newgamebutton.Size = New System.Drawing.Size(100, 100)
+        Me.newgamebutton.TabIndex = 14
+        Me.newgamebutton.Text = "New Game"
+        Me.newgamebutton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 461)
+        Me.Controls.Add(Me.newgamebutton)
+        Me.Controls.Add(Me.resetbutton)
+        Me.Controls.Add(Me.oscore)
+        Me.Controls.Add(Me.xscore)
+        Me.Controls.Add(Me.playerolabel)
+        Me.Controls.Add(Me.playerxlabel)
+        Me.Controls.Add(Me.space9)
+        Me.Controls.Add(Me.space8)
+        Me.Controls.Add(Me.space7)
+        Me.Controls.Add(Me.space6)
+        Me.Controls.Add(Me.space5)
+        Me.Controls.Add(Me.space4)
+        Me.Controls.Add(Me.space3)
+        Me.Controls.Add(Me.space2)
+        Me.Controls.Add(Me.space1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents space1 As Button
+    Friend WithEvents space2 As Button
+    Friend WithEvents space3 As Button
+    Friend WithEvents space6 As Button
+    Friend WithEvents space5 As Button
+    Friend WithEvents space4 As Button
+    Friend WithEvents space9 As Button
+    Friend WithEvents space8 As Button
+    Friend WithEvents space7 As Button
+    Friend WithEvents playerxlabel As Label
+    Friend WithEvents playerolabel As Label
+    Friend WithEvents xscore As Label
+    Friend WithEvents oscore As Label
+    Friend WithEvents resetbutton As Button
+    Friend WithEvents newgamebutton As Button
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,255 @@
+﻿Public Class Form1
+    Dim checkforXorO As Boolean = False
+    Dim AddOneToScore As Byte = 0
+
+    Sub buttonsEnabledFalse()
+        space1.Enabled = False
+        space2.Enabled = False
+        space3.Enabled = False
+        space4.Enabled = False
+        space5.Enabled = False
+        space6.Enabled = False
+        space7.Enabled = False
+        space8.Enabled = False
+        space9.Enabled = False
+
+    End Sub
+
+    Sub checkForWin()
+        'X win conditions
+        If space1.Text = "X" And space2.Text = "X" And space3.Text = "X" Then
+            space1.BackColor = Color.Black
+            space2.BackColor = Color.Black
+            space3.BackColor = Color.Black
+            MessageBox.Show("PLAYER X WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(xscore.Text)
+            xscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space4.Text = "X" And space5.Text = "X" And space6.Text = "X" Then
+            space4.BackColor = Color.Black
+            space5.BackColor = Color.Black
+            space6.BackColor = Color.Black
+            MessageBox.Show("PLAYER X WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(xscore.Text)
+            xscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space7.Text = "X" And space8.Text = "X" And space9.Text = "X" Then
+            space7.BackColor = Color.Black
+            space8.BackColor = Color.Black
+            space9.BackColor = Color.Black
+            MessageBox.Show("PLAYER X WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(xscore.Text)
+            xscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space1.Text = "X" And space4.Text = "X" And space7.Text = "X" Then
+            space1.BackColor = Color.Black
+            space4.BackColor = Color.Black
+            space7.BackColor = Color.Black
+            MessageBox.Show("PLAYER X WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(xscore.Text)
+            xscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space2.Text = "X" And space5.Text = "X" And space8.Text = "X" Then
+            space2.BackColor = Color.Black
+            space5.BackColor = Color.Black
+            space8.BackColor = Color.Black
+            MessageBox.Show("PLAYER X WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(xscore.Text)
+            xscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space3.Text = "X" And space6.Text = "X" And space9.Text = "X" Then
+            space3.BackColor = Color.Black
+            space6.BackColor = Color.Black
+            space9.BackColor = Color.Black
+            MessageBox.Show("PLAYER X WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(xscore.Text)
+            xscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space1.Text = "X" And space5.Text = "X" And space9.Text = "X" Then
+            space1.BackColor = Color.Black
+            space5.BackColor = Color.Black
+            space9.BackColor = Color.Black
+            MessageBox.Show("PLAYER X WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(xscore.Text)
+            xscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space3.Text = "X" And space5.Text = "X" And space7.Text = "X" Then
+            space3.BackColor = Color.Black
+            space5.BackColor = Color.Black
+            space7.BackColor = Color.Black
+            MessageBox.Show("PLAYER X WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(xscore.Text)
+            xscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        'O win conditions
+        If space1.Text = "O" And space2.Text = "O" And space3.Text = "O" Then
+            space1.BackColor = Color.Black
+            space2.BackColor = Color.Black
+            space3.BackColor = Color.Black
+            MessageBox.Show("PLAYER O WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(oscore.Text)
+            oscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space4.Text = "O" And space5.Text = "O" And space6.Text = "O" Then
+            space4.BackColor = Color.Black
+            space5.BackColor = Color.Black
+            space6.BackColor = Color.Black
+            MessageBox.Show("PLAYER O WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(oscore.Text)
+            oscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space7.Text = "O" And space8.Text = "O" And space9.Text = "O" Then
+            space7.BackColor = Color.Black
+            space8.BackColor = Color.Black
+            space9.BackColor = Color.Black
+            MessageBox.Show("PLAYER O WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(oscore.Text)
+            oscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space1.Text = "O" And space4.Text = "O" And space7.Text = "O" Then
+            space1.BackColor = Color.Black
+            space4.BackColor = Color.Black
+            space7.BackColor = Color.Black
+            MessageBox.Show("PLAYER O WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(oscore.Text)
+            oscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space2.Text = "O" And space5.Text = "O" And space8.Text = "O" Then
+            space2.BackColor = Color.Black
+            space5.BackColor = Color.Black
+            space8.BackColor = Color.Black
+            MessageBox.Show("PLAYER O WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(oscore.Text)
+            oscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space3.Text = "O" And space6.Text = "O" And space9.Text = "O" Then
+            space3.BackColor = Color.Black
+            space6.BackColor = Color.Black
+            space9.BackColor = Color.Black
+            MessageBox.Show("PLAYER O WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(oscore.Text)
+            oscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space1.Text = "O" And space5.Text = "O" And space9.Text = "O" Then
+            space1.BackColor = Color.Black
+            space5.BackColor = Color.Black
+            space9.BackColor = Color.Black
+            MessageBox.Show("PLAYER O WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(oscore.Text)
+            oscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+        If space3.Text = "O" And space5.Text = "O" And space7.Text = "O" Then
+            space3.BackColor = Color.Black
+            space5.BackColor = Color.Black
+            space7.BackColor = Color.Black
+            MessageBox.Show("PLAYER O WINS!", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            AddOneToScore = Convert.ToInt64(oscore.Text)
+            oscore.Text = Convert.ToString(AddOneToScore + 1)
+            buttonsEnabledFalse()
+        End If
+    End Sub
+
+    'space clicks
+    Private Sub space_click(sender As Object, e As EventArgs) Handles space9.Click, space8.Click, space7.Click, space6.Click, space5.Click, space4.Click, space3.Click, space2.Click, space1.Click
+        Dim b As Button = sender
+        If checkforXorO = False Then
+            b.Text = "X"
+            checkforXorO = True
+        Else
+            b.Text = "O"
+            checkforXorO = False
+        End If
+        checkForWin()
+        b.Enabled = False
+    End Sub
+
+    'reset button
+    Private Sub resetbutton_Click(sender As Object, e As EventArgs) Handles resetbutton.Click
+        space1.Enabled = True
+        space2.Enabled = True
+        space3.Enabled = True
+        space4.Enabled = True
+        space5.Enabled = True
+        space6.Enabled = True
+        space7.Enabled = True
+        space8.Enabled = True
+        space9.Enabled = True
+
+        'space text resets
+        space1.Text = ""
+        space2.Text = ""
+        space3.Text = ""
+        space4.Text = ""
+        space5.Text = ""
+        space6.Text = ""
+        space7.Text = ""
+        space8.Text = ""
+        space9.Text = ""
+
+        'space color reverts to white
+        space1.BackColor = Color.White
+        space2.BackColor = Color.White
+        space3.BackColor = Color.White
+        space4.BackColor = Color.White
+        space5.BackColor = Color.White
+        space6.BackColor = Color.White
+        space7.BackColor = Color.White
+        space8.BackColor = Color.White
+        space9.BackColor = Color.White
+
+    End Sub
+
+    Private Sub newgamebutton_Click(sender As Object, e As EventArgs) Handles newgamebutton.Click
+        space1.Enabled = True
+        space2.Enabled = True
+        space3.Enabled = True
+        space4.Enabled = True
+        space5.Enabled = True
+        space6.Enabled = True
+        space7.Enabled = True
+        space8.Enabled = True
+        space9.Enabled = True
+
+        'space text resets
+        space1.Text = ""
+        space2.Text = ""
+        space3.Text = ""
+        space4.Text = ""
+        space5.Text = ""
+        space6.Text = ""
+        space7.Text = ""
+        space8.Text = ""
+        space9.Text = ""
+
+        'space color reverts to white
+        space1.BackColor = Color.White
+        space2.BackColor = Color.White
+        space3.BackColor = Color.White
+        space4.BackColor = Color.White
+        space5.BackColor = Color.White
+        space6.BackColor = Color.White
+        space7.BackColor = Color.White
+        space8.BackColor = Color.White
+        space9.BackColor = Color.White
+
+        'scores revert to 0
+        oscore.Text = "0"
+        xscore.Text = "0"
+    End Sub
+End Class
+
